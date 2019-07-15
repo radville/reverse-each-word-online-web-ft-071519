5d727d4dc5edd0478d33253685e81630d29452e0
Laura Radville <laura.radville@gmail.com>
2019-07-15 18:38:10 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -10,7 +10,7 @@ end
 def reverse_each_word(string)
   array = string.split(" ")
   array.collect do |word|
-    new << word.reverse
+    word.reverse
   end
-  new.join(" ")
+  # new.join(" ")
 end
